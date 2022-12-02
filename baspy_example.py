c14fc2ebc9bb26705e4343cee2584d025e5aae8d
rywill93 <97890185+rywill93@users.noreply.github.com>
2022-12-02 15:25:39 +0000

    Update baspy_example.py

--- baspy_example.py
+++ baspy_example.py
@@ -5,21 +5,18 @@ import netCDF4
 from netCDF4 import Dataset
 import xarray as xr
 
-# Search catalogue of data on CEDA and store results in a dataframe - Arguments can give: 
-df = bp.catalogue(dataset='cmip6', Model='EC-Earth3', Experiment=['historical','ssp370','ssp585'], CMOR = 'Amon', Var = 'tas')
+# Search catalogue of data on CEDA and store results in a dataframe 
+
+# Suppose we want monthly 2m air temperature for all available models for historical, ssp370 and ssp585
+df = bp.catalogue(dataset='cmip6', Experiment=['historical','ssp370','ssp585'], CMOR = 'Amon', Var = 'tas') # E.g. Could add if we just wanted for one model: EC-Earth3 (Model='EC-Earth3')
 
 # Save dataframe search results to CSV file
-df.to_csv(path_or_buf='/home/users/rywill93/baspy_output.csv', index=False) # Output all model run metadata to CSV
+df.to_csv(path_or_buf='/home/users/rywill93/baspy_output.csv', index=False) # Output all model run metadata to CSV (Remember to change filepath)
 
-i = -1 # Keep track of model runs included
-
-### Iterate over rows in catalogue - We may only be interested in the first available ensemble member for historical, ssp370 and ssp585
+### Iterate over rows in catalogue - We may only be interested in the first available ensemble member for historical, ssp370 and ssp585 ###
 for index, row in df.iterrows():  
 
-    i = i + 1
-    print (i)
-
-    # Refer to model column storing the Experiment ID
+    # Refer to model column storing the Experiment ID (as shown in CSV output file)
     if df.iloc[i,3] == df.iloc[i-1,3]:
     
         continue # Ignore row if model is same as in row before (we only want want the first available ensemble member for each experiment)
@@ -31,7 +28,7 @@ for index, row in df.iterrows():
         myfile = bp.open_dataset(row, use_cftime = 'True') # xarray
         #myfile = bp.get_cube(row) # iris
 
-        #print(myfile)
+        print(myfile) # Show metadata
 
         # Read in Variables
         tas = myfile.tas 
